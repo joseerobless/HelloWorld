c30ed652d67545a07326e869dda200ab2a62608c
joseerobless <joseerobles@gmail.com>
2022-02-02 15:28:40 -0700

    Hello

---
+++ HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => {
+  return <h1>Hello World</h1>;
+};
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('react-render-example')
+);
+
+export default HelloWorld;
