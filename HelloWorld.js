94bcaf29c149c4b164b7fdd11d80a7f9224e43b0
joseerobless <joseerobles@gmail.com>
2022-02-02 16:05:02 -0700

    J

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,5 +1,5 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const HelloWorld = () => {
   return <h1>Hello World</h1>;
