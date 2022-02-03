075624de28e9a6d97ce322cac0239a4d522f3e64
joseerobless <joseerobles@gmail.com>
2022-02-03 15:27:57 -0700

    Create HelloWorld3.js

---
+++ HelloWorld3.js
@@ -0,0 +1,13 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const HelloWorld = () => {
+  return Hello World;
+};
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('react-render-example')
+);
+
+export default HelloWorld;
